26a0b9a4d661a68335bc8585de1b5a5f2ac2d2b1
Carlos Palomá <c.paloma@uniandes.edu.co>
2022-11-26 19:05:03 -0500

    cambiar colores de la interfaz

--- Interfaz_P_try1.py
+++ Interfaz_P_try1.py
@@ -1,3 +1,4 @@
+#%%
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
@@ -25,7 +26,7 @@ etiqueta.pack(side=tk.TOP)
 
 frame1 = tk.Frame(master=window)
 frame1.place(x=25, y=400)
-frame1.config(bg="#F4D03F", width=850, height=300, relief=tk.GROOVE, bd=8)
+frame1.config(bg="#83D6B7", width=850, height=300, relief=tk.GROOVE, bd=8)
 
 
 
@@ -39,12 +40,12 @@ def CerrarAplicacion():
 
 
 Style = ttk.Style()      
-Style.configure('2.TButton', font =('Times', 10, 'bold', 'underline'), foreground = '#E88A18')
+Style.configure('2.TButton', font =('Times', 10, 'bold', 'underline'), foreground = '#000000')
 Style.configure('titulo', font =('Times', 20, 'bold', 'underline'), foreground = '#E88A18')
 
 
 Style.map("1.TButton",
-          foreground=[('pressed', 'orange'), ('active', '#1A5276')],
+          foreground=[('pressed', 'orange'), ('active', '#000000')],
           background=[('pressed', '!disabled', 'yellow'), ('active', 'yellow')])
 
 Style.map("titulo",
@@ -58,14 +59,14 @@ Boton4 = ttk.Button(master=window, text="Exportar", style="2.TButton", command =
 
 #Textos en la interfaz
 
-lbl_titulo = tk.Label(master=frame1, bg="#F4D03F", font=('TIMES', 15, 'bold'), text="Método solución").place(x=5,y=5)
+lbl_titulo = tk.Label(master=frame1, bg="#83D6B7", font=('TIMES', 15, 'bold'), text="Método solución").place(x=5,y=5)
 
 #Checkbox
-c1 = tk.Checkbutton(master=frame1, bg="#F4D03F", font=('TIMES', 11, 'bold italic'),text="Runge-Kutta 2",variable=1, onvalue=1, offvalue=0, command=0).place(x=20,y=50)
-c2 = tk.Checkbutton(master=frame1, bg="#F4D03F", font=('TIMES', 11, 'bold italic'),text="Runge-Kutta 4",variable=2, onvalue=1, offvalue=0, command=0).place(x=20,y=80)
-c3 = tk.Checkbutton(master=frame1, bg="#F4D03F", font=('TIMES', 11, 'bold italic'),text="Euler Adelante",variable=3, onvalue=1, offvalue=0, command=0).place(x=20,y=110)
-c4 = tk.Checkbutton(master=frame1, bg="#F4D03F", font=('TIMES', 11, 'bold italic'),text="Euler Modificado",variable=4, onvalue=1, offvalue=0, command=0).place(x=20,y=140)
-c5 = tk.Checkbutton(master=frame1, bg="#F4D03F", font=('TIMES', 11, 'bold italic'),text="Euler Atrás",variable=5, onvalue=1, offvalue=0, command=0).place(x=20,y=170)
+c1 = tk.Checkbutton(master=frame1, bg="#83D6B7", font=('TIMES', 11, 'bold italic'),text="Runge-Kutta 2",variable=1, onvalue=1, offvalue=0, command=0).place(x=20,y=50)
+c2 = tk.Checkbutton(master=frame1, bg="#83D6B7", font=('TIMES', 11, 'bold italic'),text="Runge-Kutta 4",variable=2, onvalue=1, offvalue=0, command=0).place(x=20,y=80)
+c3 = tk.Checkbutton(master=frame1, bg="#83D6B7", font=('TIMES', 11, 'bold italic'),text="Euler Adelante",variable=3, onvalue=1, offvalue=0, command=0).place(x=20,y=110)
+c4 = tk.Checkbutton(master=frame1, bg="#83D6B7", font=('TIMES', 11, 'bold italic'),text="Euler Modificado",variable=4, onvalue=1, offvalue=0, command=0).place(x=20,y=140)
+c5 = tk.Checkbutton(master=frame1, bg="#83D6B7", font=('TIMES', 11, 'bold italic'),text="Euler Atrás",variable=5, onvalue=1, offvalue=0, command=0).place(x=20,y=170)
 
 #c1.pack()
 #lbl_x = tk.Label(master=frame1,font=('math', 15, 'bold italic'),bg="#F4D03F",text="x =",relief = tk.RIDGE, borderwidth = 3,width = 8).place(x=200, y=100)
@@ -73,3 +74,4 @@ c5 = tk.Checkbutton(master=frame1, bg="#F4D03F", font=('TIMES', 11, 'bold italic
 
 
 window.mainloop()
+# %%
